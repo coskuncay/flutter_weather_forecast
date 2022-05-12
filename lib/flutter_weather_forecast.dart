5db6f519a9a25ed170eca0ea5feacd50421f168c
Emre Coşkunçay <emrecoskuncay@gmail.com>
2022-05-12 14:03:36 +0300

    endpoints added

--- lib/flutter_weather_forecast.dart
+++ lib/flutter_weather_forecast.dart
@@ -1,5 +1,11 @@
 library flutter_weather_forecast;
 
+import 'dart:convert';
+
+import 'package:flutter_weather_forecast/lang/tr_TR.dart';
+import 'package:flutter_weather_forecast/models/forecast_model.dart';
+import 'package:flutter_weather_forecast/models/status_model.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class WeatherForecast extends StatefulWidget {
@@ -10,88 +16,152 @@ class WeatherForecast extends StatefulWidget {
 }
 
 class _WeatherForecastState extends State<WeatherForecast> {
+  bool isLoading = false;
   @override
   void initState() {
     super.initState();
+  }
+
+  void getForecastDaily() async {
+    final queryParameters = {
+      'istno': '90601',
+    };
+    final uri = Uri.https(
+        'servis.mgm.gov.tr', '/web/tahminler/gunluk', queryParameters);
+    var response =
+        await http.get(uri, headers: {'origin': 'https://www.mgm.gov.tr'});
+    ForecastModel dailyForecast =
+        ForecastModel.fromJsonModel(json.decode(response.body)[0]);
+    print('Response status: ${response.statusCode}');
+    print(dailyForecast.enDusukGun1);
+    print(dailyForecast.enYuksekGun1);
+  }
+
+  void getCurrentForecast() async {
+    final queryParameters = {
+      'merkezid': '90601',
+    };
+    final uri =
+        Uri.https('servis.mgm.gov.tr', '/web/sondurumlar', queryParameters);
+    var response =
+        await http.get(uri, headers: {'origin': 'https://www.mgm.gov.tr'});
+    StatusModel currentForecast =
+        StatusModel.fromJsonModel(json.decode(response.body)[0]);
+    print('Response status: ${response.statusCode}');
+    print(currentForecast.hadiseKodu);
+    print(tr_TR[currentForecast.hadiseKodu.toLowerCase()]);
+    print(currentForecast.sicaklik.toStringAsFixed(1));
+    print(currentForecast.ruzgarHiz.toStringAsFixed(2));
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * .24,
-        child: Column(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height * .18,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5),
-                color: const Color.fromRGBO(255, 255, 255, 1),
-                border: Border.all(
-                  color: const Color.fromARGB(255, 177, 177, 177),
-                  width: 1,
-                ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * .15,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: const Color.fromRGBO(255, 255, 255, 1),
+              border: Border.all(
+                color: const Color.fromARGB(255, 216, 215, 216),
+                width: 1,
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: const [
-                        Icon(
-                          Icons.refresh,
-                          size: 15,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      isLoading
+                          ? const SizedBox(
+                              width: 10,
+                              height: 10,
+                              child: CircularProgressIndicator(
+                                strokeWidth: 1,
+                                color: Color.fromARGB(255, 177, 177, 177),
+                              ),
+                            )
+                          : InkWell(
+                              onTap: () {
+                                setState(
+                                  () {
+                                    // isLoading = true;
+                                    getCurrentForecast();
+                                  },
+                                );
+                              },
+                              child: const Icon(
+                                Icons.refresh,
+                                size: 25,
+                                color: Color.fromARGB(255, 177, 177, 177),
+                              ),
+                            ),
+                      const Text(
+                        '12.05.2022 - 14:03',
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
                           color: Color.fromARGB(255, 177, 177, 177),
+                          fontFamily: 'Montserrat',
+                          fontSize: 11,
+                          fontWeight: FontWeight.normal,
                         ),
-                        Text(
-                          '12.04.2022-14:03',
-                          textAlign: TextAlign.right,
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 177, 177, 177),
-                            fontFamily: 'Montserrat',
-                            fontSize: 11,
-                            fontWeight: FontWeight.normal,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Image.asset(
+                        'assets/images/y.png',
+                        package: 'flutter_weather_forecast',
+                        scale: .8,
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          // Figma Flutter Generator GneliWidget - TEXT
+                          Text(
+                            'Güneşli',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: Color.fromRGBO(45, 49, 49, 1),
+                              fontFamily: 'Montserrat',
+                              fontSize: 14,
+                              letterSpacing:
+                                  0 /*percentages not used in flutter. defaulting to zero*/,
+                              fontWeight: FontWeight.normal,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const [
-                            // Figma Flutter Generator GneliWidget - TEXT
-                            Text(
-                              'Güneşli',
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                color: Color.fromRGBO(45, 49, 49, 1),
-                                fontFamily: 'Montserrat',
-                                fontSize: 14,
-                                letterSpacing:
-                                    0 /*percentages not used in flutter. defaulting to zero*/,
-                                fontWeight: FontWeight.normal,
-                              ),
+                          Text(
+                            '18 °C',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.normal,
+                              height: 1,
                             ),
-                            Text(
-                              '18 °C',
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                fontSize: 40,
-                                fontWeight: FontWeight.normal,
-                                height: 1,
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          width: 20,
-                        ),
-                        Column(
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        width: 25,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Row(
                               children: const [
@@ -99,26 +169,24 @@ class _WeatherForecastState extends State<WeatherForecast> {
                                   'Min:',
                                   textAlign: TextAlign.right,
                                   style: TextStyle(
-                                      color: Color.fromRGBO(172, 174, 186, 1),
-                                      fontFamily: 'Montserrat',
-                                      fontSize: 10,
-                                      letterSpacing:
-                                          0 /*percentages not used in flutter. defaulting to zero*/,
-                                      fontWeight: FontWeight.normal,
-                                      height: 1),
+                                    color: Color.fromRGBO(172, 174, 186, 1),
+                                    fontSize: 13,
+                                    fontStyle: FontStyle.italic,
+                                  ),
                                 ),
                                 Text(
                                   '3 °C',
                                   style: TextStyle(
-                                      color: Color.fromRGBO(45, 49, 49, 1),
-                                      fontFamily: 'Montserrat',
-                                      fontSize: 11,
-                                      letterSpacing:
-                                          0 /*percentages not used in flutter. defaulting to zero*/,
-                                      fontWeight: FontWeight.normal,
-                                      height: 1),
+                                    color: Color.fromRGBO(45, 49, 49, 1),
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 12,
+                                    fontStyle: FontStyle.italic,
+                                  ),
                                 ),
                               ],
+                            ),
+                            const SizedBox(
+                              height: 10,
                             ),
                             Row(
                               children: const [
@@ -126,91 +194,90 @@ class _WeatherForecastState extends State<WeatherForecast> {
                                   'Max:',
                                   textAlign: TextAlign.right,
                                   style: TextStyle(
-                                      color: Color.fromRGBO(172, 174, 186, 1),
-                                      fontFamily: 'Montserrat',
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.normal,
-                                      height: 1),
+                                    color: Color.fromRGBO(172, 174, 186, 1),
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 13,
+                                    fontStyle: FontStyle.italic,
+                                  ),
                                 ),
                                 Text(
                                   '21 °C',
                                   style: TextStyle(
-                                      color: Color.fromRGBO(45, 49, 49, 1),
-                                      fontFamily: 'Montserrat',
-                                      fontSize: 11,
-                                      fontWeight: FontWeight.normal,
-                                      height: 1),
+                                    color: Color.fromRGBO(45, 49, 49, 1),
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 12,
+                                    fontStyle: FontStyle.italic,
+                                  ),
                                 ),
                               ],
                             ),
                           ],
                         ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: const [
-                                Text(
-                                  'Rüzgar:',
-                                  textAlign: TextAlign.right,
-                                  style: TextStyle(
-                                      color: Color.fromRGBO(172, 174, 186, 1),
-                                      fontFamily: 'Montserrat',
-                                      fontSize: 10,
-                                      letterSpacing:
-                                          0 /*percentages not used in flutter. defaulting to zero*/,
-                                      fontWeight: FontWeight.normal,
-                                      height: 1),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            children: const [
+                              Text(
+                                'Rüzgar',
+                                textAlign: TextAlign.right,
+                                style: TextStyle(
+                                  color: Color.fromRGBO(172, 174, 186, 1),
+                                  fontSize: 13,
+                                  fontStyle: FontStyle.italic,
                                 ),
-                                Text(
-                                  '12km/h',
-                                  style: TextStyle(
-                                      color: Color.fromRGBO(45, 49, 49, 1),
-                                      fontFamily: 'Montserrat',
-                                      fontSize: 11,
-                                      letterSpacing:
-                                          0 /*percentages not used in flutter. defaulting to zero*/,
-                                      fontWeight: FontWeight.normal,
-                                      height: 1),
+                              ),
+                              Text(
+                                '12km/h',
+                                style: TextStyle(
+                                  color: Color.fromRGBO(45, 49, 49, 1),
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 12,
+                                  fontStyle: FontStyle.italic,
                                 ),
-                              ],
-                            ),
-                            Row(
-                              children: const [
-                                Text(
-                                  'Nem:',
-                                  textAlign: TextAlign.right,
-                                  style: TextStyle(
-                                      color: Color.fromRGBO(172, 174, 186, 1),
-                                      fontFamily: 'Montserrat',
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.normal,
-                                      height: 1),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: const [
+                              Text(
+                                'Nem:',
+                                textAlign: TextAlign.right,
+                                style: TextStyle(
+                                  color: Color.fromRGBO(172, 174, 186, 1),
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 13,
+                                  fontStyle: FontStyle.italic,
                                 ),
-                                Text(
-                                  '12%',
-                                  style: TextStyle(
-                                      color: Color.fromRGBO(45, 49, 49, 1),
-                                      fontFamily: 'Montserrat',
-                                      fontSize: 11,
-                                      fontWeight: FontWeight.normal,
-                                      height: 1),
+                              ),
+                              Text(
+                                '12%',
+                                style: TextStyle(
+                                  color: Color.fromRGBO(45, 49, 49, 1),
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 12,
+                                  fontStyle: FontStyle.italic,
                                 ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/models/status_model.dart
+++ lib/models/status_model.dart
@@ -15,15 +15,15 @@ class StatusModel {
   String rasatTaf;
   double ruzgarHiz;
   int ruzgarYon;
-  int sicaklik;
-  DateTime veriZamani;
+  double sicaklik;
+  String veriZamani;
   double yagis00Now;
   int yagis10Dk;
   double yagis12Saat;
   int yagis1Saat;
   double yagis24Saat;
   double yagis6Saat;
-  DateTime denizVeriZamani;
+  String denizVeriZamani;
   StatusModel({
     required this.aktuelBasinc,
     required this.denizSicaklik,
@@ -50,58 +50,8 @@ class StatusModel {
     required this.denizVeriZamani,
   });
 
-  StatusModel copyWith({
-    double? aktuelBasinc,
-    int? denizSicaklik,
-    double? denizeIndirgenmisBasinc,
-    int? gorus,
-    String? hadiseKodu,
-    int? istNo,
-    int? kapalilik,
-    int? karYukseklik,
-    int? nem,
-    String? rasatMetar,
-    String? rasatSinoptik,
-    String? rasatTaf,
-    double? ruzgarHiz,
-    int? ruzgarYon,
-    int? sicaklik,
-    DateTime? veriZamani,
-    double? yagis00Now,
-    int? yagis10Dk,
-    double? yagis12Saat,
-    int? yagis1Saat,
-    double? yagis24Saat,
-    double? yagis6Saat,
-    DateTime? denizVeriZamani,
-  }) {
-    return StatusModel(
-      aktuelBasinc: aktuelBasinc ?? this.aktuelBasinc,
-      denizSicaklik: denizSicaklik ?? this.denizSicaklik,
-      denizeIndirgenmisBasinc:
-          denizeIndirgenmisBasinc ?? this.denizeIndirgenmisBasinc,
-      gorus: gorus ?? this.gorus,
-      hadiseKodu: hadiseKodu ?? this.hadiseKodu,
-      istNo: istNo ?? this.istNo,
-      kapalilik: kapalilik ?? this.kapalilik,
-      karYukseklik: karYukseklik ?? this.karYukseklik,
-      nem: nem ?? this.nem,
-      rasatMetar: rasatMetar ?? this.rasatMetar,
-      rasatSinoptik: rasatSinoptik ?? this.rasatSinoptik,
-      rasatTaf: rasatTaf ?? this.rasatTaf,
-      ruzgarHiz: ruzgarHiz ?? this.ruzgarHiz,
-      ruzgarYon: ruzgarYon ?? this.ruzgarYon,
-      sicaklik: sicaklik ?? this.sicaklik,
-      veriZamani: veriZamani ?? this.veriZamani,
-      yagis00Now: yagis00Now ?? this.yagis00Now,
-      yagis10Dk: yagis10Dk ?? this.yagis10Dk,
-      yagis12Saat: yagis12Saat ?? this.yagis12Saat,
-      yagis1Saat: yagis1Saat ?? this.yagis1Saat,
-      yagis24Saat: yagis24Saat ?? this.yagis24Saat,
-      yagis6Saat: yagis6Saat ?? this.yagis6Saat,
-      denizVeriZamani: denizVeriZamani ?? this.denizVeriZamani,
-    );
-  }
+  static StatusModel fromJsonModel(Map<String, dynamic> map) =>
+      StatusModel.fromMap(map);
 
   Map<String, dynamic> toMap() {
     return {
@@ -120,14 +70,14 @@ class StatusModel {
       'ruzgarHiz': ruzgarHiz,
       'ruzgarYon': ruzgarYon,
       'sicaklik': sicaklik,
-      'veriZamani': veriZamani.millisecondsSinceEpoch,
+      'veriZamani': veriZamani,
       'yagis00Now': yagis00Now,
       'yagis10Dk': yagis10Dk,
       'yagis12Saat': yagis12Saat,
       'yagis1Saat': yagis1Saat,
       'yagis24Saat': yagis24Saat,
       'yagis6Saat': yagis6Saat,
-      'denizVeriZamani': denizVeriZamani.millisecondsSinceEpoch,
+      'denizVeriZamani': denizVeriZamani,
     };
   }
 
@@ -148,16 +98,15 @@ class StatusModel {
       rasatTaf: map['rasatTaf'] ?? '',
       ruzgarHiz: map['ruzgarHiz']?.toDouble() ?? 0.0,
       ruzgarYon: map['ruzgarYon']?.toInt() ?? 0,
-      sicaklik: map['sicaklik']?.toInt() ?? 0,
-      veriZamani: DateTime.fromMillisecondsSinceEpoch(map['veriZamani']),
+      sicaklik: map['sicaklik']?.toDouble() ?? 0.0,
+      veriZamani: map['veriZamani'] ?? '',
       yagis00Now: map['yagis00Now']?.toDouble() ?? 0.0,
       yagis10Dk: map['yagis10Dk']?.toInt() ?? 0,
       yagis12Saat: map['yagis12Saat']?.toDouble() ?? 0.0,
       yagis1Saat: map['yagis1Saat']?.toInt() ?? 0,
       yagis24Saat: map['yagis24Saat']?.toDouble() ?? 0.0,
       yagis6Saat: map['yagis6Saat']?.toDouble() ?? 0.0,
-      denizVeriZamani:
-          DateTime.fromMillisecondsSinceEpoch(map['denizVeriZamani']),
+      denizVeriZamani: map['denizVeriZamani'] ?? '',
     );
   }
 
@@ -165,66 +114,4 @@ class StatusModel {
 
   factory StatusModel.fromJson(String source) =>
       StatusModel.fromMap(json.decode(source));
-
-  @override
-  String toString() {
-    return 'StatusModel(aktuelBasinc: $aktuelBasinc, denizSicaklik: $denizSicaklik, denizeIndirgenmisBasinc: $denizeIndirgenmisBasinc, gorus: $gorus, hadiseKodu: $hadiseKodu, istNo: $istNo, kapalilik: $kapalilik, karYukseklik: $karYukseklik, nem: $nem, rasatMetar: $rasatMetar, rasatSinoptik: $rasatSinoptik, rasatTaf: $rasatTaf, ruzgarHiz: $ruzgarHiz, ruzgarYon: $ruzgarYon, sicaklik: $sicaklik, veriZamani: $veriZamani, yagis00Now: $yagis00Now, yagis10Dk: $yagis10Dk, yagis12Saat: $yagis12Saat, yagis1Saat: $yagis1Saat, yagis24Saat: $yagis24Saat, yagis6Saat: $yagis6Saat, denizVeriZamani: $denizVeriZamani)';
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-
-    return other is StatusModel &&
-        other.aktuelBasinc == aktuelBasinc &&
-        other.denizSicaklik == denizSicaklik &&
-        other.denizeIndirgenmisBasinc == denizeIndirgenmisBasinc &&
-        other.gorus == gorus &&
-        other.hadiseKodu == hadiseKodu &&
-        other.istNo == istNo &&
-        other.kapalilik == kapalilik &&
-        other.karYukseklik == karYukseklik &&
-        other.nem == nem &&
-        other.rasatMetar == rasatMetar &&
-        other.rasatSinoptik == rasatSinoptik &&
-        other.rasatTaf == rasatTaf &&
-        other.ruzgarHiz == ruzgarHiz &&
-        other.ruzgarYon == ruzgarYon &&
-        other.sicaklik == sicaklik &&
-        other.veriZamani == veriZamani &&
-        other.yagis00Now == yagis00Now &&
-        other.yagis10Dk == yagis10Dk &&
-        other.yagis12Saat == yagis12Saat &&
-        other.yagis1Saat == yagis1Saat &&
-        other.yagis24Saat == yagis24Saat &&
-        other.yagis6Saat == yagis6Saat &&
-        other.denizVeriZamani == denizVeriZamani;
-  }
-
-  @override
-  int get hashCode {
-    return aktuelBasinc.hashCode ^
-        denizSicaklik.hashCode ^
-        denizeIndirgenmisBasinc.hashCode ^
-        gorus.hashCode ^
-        hadiseKodu.hashCode ^
-        istNo.hashCode ^
-        kapalilik.hashCode ^
-        karYukseklik.hashCode ^
-        nem.hashCode ^
-        rasatMetar.hashCode ^
-        rasatSinoptik.hashCode ^
-        rasatTaf.hashCode ^
-        ruzgarHiz.hashCode ^
-        ruzgarYon.hashCode ^
-        sicaklik.hashCode ^
-        veriZamani.hashCode ^
-        yagis00Now.hashCode ^
-        yagis10Dk.hashCode ^
-        yagis12Saat.hashCode ^
-        yagis1Saat.hashCode ^
-        yagis24Saat.hashCode ^
-        yagis6Saat.hashCode ^
-        denizVeriZamani.hashCode;
-  }
 }
